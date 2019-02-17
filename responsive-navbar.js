7ebdbadcce03a996d4a3f21358259b121d8f3d32
Tiffany Ray <tiffanyraylee@gmail.com>
2019-02-17 11:28:16 -0600

    Updated the nav to look and function better

--- responsive-navbar.js
+++ responsive-navbar.js
@@ -1,9 +1,14 @@
-const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav_links');
-// toggle Nav
-  burger.addEventListener('click', () => {
-    nav.classList.toggle('nav_active');
+
+$(document).ready(() => {
+
+  $('#mobile-menu').on('click', () => {
+    let $navUl = $('.nav-ul');
+    $navUl.slideToggle('slow');
   });
-}
-navSlide();
+
+  $(window).bind("resize", () => {
+    if( $(window).width() > 900) {
+      $('.nav-ul').removeAttr("style");
+    }
+  })
+});
